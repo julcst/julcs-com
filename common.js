7aae6c42babb963ea2cb859d26a6e7d65c41218f
Julian C. Stamm <juliancstamm@gmail.com>
2023-06-04 18:00:54 +0200

    Fix flashing and mistyping

--- common.js
+++ common.js
@@ -1,14 +1,14 @@
 // Register type effect
-window.onload = function() {
-    Array.from(document.getElementsByClassName("typed")).forEach(typeEffect);
-}
+Array.from(document.getElementsByClassName("typed")).forEach(typeEffect);
 
 function typeEffect(typebox) {
-    text = typebox.innerText;
-    typebox.innerText = "";
-    for (let i = 0; i < text.length; i++) {
+    target = typebox.innerText;
+    text = "";
+    typebox.innerText = text;
+    for (let i = 0; i < target.length; i++) {
         setTimeout(() => {
-            typebox.innerText += text[i];
+            text += target[i];
+            typebox.innerText = text;
         }, 30 * i);
     }
 }
