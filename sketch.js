5c4190a8f5612e432f0d202e48d22c9a2457af98
Julian C. Stamm <juliancstamm@gmail.com>
2023-06-14 09:56:23 +0200

    Remove antialiasing

--- sketch.js
+++ sketch.js
@@ -6,17 +6,11 @@ const n = 67;
 // Render 2d point grid with mxn points in WEBGL
 const canvas = document.getElementById("sketch");
 const gl = canvas.getContext("webgl", {
-    premultipliedAlpha: false,
     alpha: true,
     antialias: false,
     depth: false,
     stencil: false
 });
-// Enable fwidth for antialiasing
-gl.getExtension("OES_standard_derivatives");
-// Enable blending
-gl.enable(gl.BLEND);
-gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
 
 function resizeCanvas() {
     canvas.width = window.devicePixelRatio * document.documentElement.clientWidth;
@@ -35,15 +29,13 @@ gl.shaderSource(vertexShader, `
     uniform float scale;
     uniform float offset;
     uniform float t;
-    varying float weight;
     void main() {
         vec2 grid = (pos - 0.5) * 2.0;
         float x = offset + t + pos.x * 10.0;
         vec2 a = vec2(sin(x * 0.3) * 0.5 + sin(x * 0.8) * 0.5, sin(x * 0.7) * 0.5 + sin(x * 0.1) * 0.5);
         vec2 b = vec2(sin(x * 0.5) * 0.5 + sin(x * 0.4) * 0.5, sin(x * 0.2) * 0.5 + sin(x * 0.9) * 0.5);
         vec2 p = mix(a, b, pos.y);
-        gl_PointSize = mix(3.0, scale, pos.y * pos.x);
-        weight = clamp(mix(0.1, scale, pos.y * pos.x), 0.0, 1.0);
+        gl_PointSize = mix(2.0, scale, pos.y * pos.x);
         gl_Position = vec4(mix(grid, p, smoothstep(3.0, 10.0, t)), 0.0, 1.0);
     }`);
 gl.compileShader(vertexShader);
@@ -54,26 +46,14 @@ if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
 // Render antialiased circles
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
 gl.shaderSource(fragmentShader, `
-    #ifdef GL_OES_standard_derivatives
-    #extension GL_OES_standard_derivatives : enable
-    #endif
     precision mediump float;
     uniform vec4 color;
     uniform vec2 res;
-    varying float weight;
     void main() {
         vec2 cxy = (gl_PointCoord - 0.5) * 2.0;
         float r = dot(cxy, cxy);
-    #ifdef GL_OES_standard_derivatives
-        float delta = fwidth(r);
-        //if (r > 1.0 + delta) discard;
-        float alpha = smoothstep(1.0, 1.0 - delta, r);
-        gl_FragColor = vec4(color.rgb * weight, color.a * alpha);
-        gl_FragColor.rgb *= gl_FragColor.a;
-    #else
         if (r > 1.0) discard;
         gl_FragColor = vec4(color.rgba);
-    #endif
     }`);
 gl.compileShader(fragmentShader);
 if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
