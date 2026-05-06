0cda7fcc1feac133ba4853bd80fc8a313c990d80
Julian Stamm <mail@julcs.com>
2026-05-06 18:52:53 +0200

    Add astro-favicons integration and simplify build process; remove unused icon generation scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import favicons from 'astro-favicons';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://julcs.com',
   build: {
     format: 'file'
   },
-  integrations: [sitemap()]
+  integrations: [sitemap(), favicons()]
 });
